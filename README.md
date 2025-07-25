017d84d4bbc6389ba08980b4302018f02c0df139
Sergey Zybnev <s.zybnev@poxek.cc>
2025-07-25 14:49:59 +0300

    many new features

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # hakip2host
-hakip2host takes a list of IP addresses via stdin, then does a series of checks to return associated domain names.
+
+hakip2host takes a list of IP addresses via stdin, file, or command line arguments, then does a series of checks to return associated domain names.
 
 Current supported checks are:
 
@@ -11,20 +12,58 @@ Current supported checks are:
 
 Install golang, then:
 
-```
-go install github.com/hakluke/hakip2host@latest
+```bash
+go install github.com/szybnev/hakip2host@latest
 ```
 
 ## Help
 
-```
+```bash
 ./hakip2host --help
 ```
 
-## Example usage
+## Usage
 
-```
+### Input via stdin (original method)
+
+```bash
 hakluke$ prips 173.0.84.0/24 | ./hakip2host
+```
+
+### Input via file (-l/--list)
+
+```bash
+hakluke$ ./hakip2host -l ips.txt
+hakluke$ ./hakip2host --list ips.txt
+```
+
+File format (ips.txt):
+
+```bash
+192.168.1.1
+10.0.0.0/24
+# Comments are ignored
+172.16.0.1
+```
+
+### Input via CLI argument (-i/--input)
+
+```bash
+hakluke$ ./hakip2host -i 192.168.1.1
+hakluke$ ./hakip2host --input 10.0.0.0/24
+```
+
+## Flags
+
+- `-h, --help` - Help menu
+- `-l, --list` - File containing list of IP addresses/CIDR blocks
+- `-i, --input` - Single IP address or CIDR block
+- `-o, --output` - Save stdout to file
+
+## Example output
+
+```bash
+$ print 173.0.84.0/24 | ./hakip2host
 [DNS-PTR] 173.0.84.23 new-creditcenter.paypal.com.
 [DNS-PTR] 173.0.84.11 slc-a-origin-www-1.paypal.com.
 [DNS-PTR] 173.0.84.10 admin.paypal.com.
--- hakip2host.go
+++ hakip2host.go
@@ -15,6 +15,148 @@ import (
 	"time"
 )
 
+const (
+	defaultWorkers = 32
+	defaultDNSProtocol = "udp"
+	defaultResolverPort = 53
+)
+
+// printUsage выводит справку по использованию
+func printUsage() {
+	fmt.Println("hakip2host - resolve IP addresses to domain names using DNS PTR and SSL certificates")
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println("  hakip2host [options]")
+	fmt.Println("  print 192.168.1.0/24 | hakip2host")
+	fmt.Println()
+	fmt.Println("Options:")
+	fmt.Println("  -i, --input <ip/cidr>    Single IP address or CIDR block")
+	fmt.Println("  -l, --list <file>        File containing IP addresses/CIDR blocks")
+	fmt.Println("  -o, --output <file>      Save results to file")
+	fmt.Println("  -s, --silent             Output only domain names")
+	fmt.Println("  -h, --help               Show this help message")
+	fmt.Println()
+	fmt.Println("Examples:")
+	fmt.Println("  hakip2host -i 8.8.8.8")
+	fmt.Println("  hakip2host -i 192.168.1.0/24")
+	fmt.Println("  hakip2host -l ips.txt")
+	fmt.Println("  hakip2host -s -i 8.8.8.8")
+	fmt.Println("  hakip2host -o results.txt -l ips.txt")
+	fmt.Println("  hakip2host -s -o domains.txt -i 8.8.8.8")
+	fmt.Println("  echo '8.8.8.8' | hakip2host")
+	fmt.Println()
+	fmt.Println("File format (one IP/CIDR per line, # for comments):")
+	fmt.Println("  192.168.1.1")
+	fmt.Println("  10.0.0.0/24")
+	fmt.Println("  # This is a comment")
+}
+
+// expandCIDR расширяет CIDR блок в список IP адресов
+func expandCIDR(cidr string) ([]string, error) {
+	if !strings.Contains(cidr, "/") {
+		// Это обычный IP адрес
+		if net.ParseIP(cidr) != nil {
+			return []string{cidr}, nil
+		}
+		return nil, fmt.Errorf("invalid IP address: %s", cidr)
+	}
+
+	_, network, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	size, _ := network.Mask.Size()
+	if size < 16 { // Слишком большой блок
+		return nil, fmt.Errorf("CIDR block too large: %s", cidr)
+	}
+
+	var ips []string
+	for ip := network.IP.Mask(network.Mask); network.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+	
+	// Удаляем network и broadcast адреса для IPv4
+	if len(ips) > 2 && strings.Contains(cidr, ".") {
+		ips = ips[1 : len(ips)-1]
+	}
+
+	return ips, nil
+}
+
+// inc увеличивает IP адрес на единицу
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
+}
+
+// readInputs читает IP адреса из разных источников
+func readInputs(inputFile, inputArg string) ([]string, error) {
+	var inputs []string
+
+	// Читаем из файла если указан флаг -l
+	if inputFile != "" {
+		file, err := os.Open(inputFile)
+		if err != nil {
+			return nil, fmt.Errorf("cannot open file %s: %v", inputFile, err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+			if line != "" && !strings.HasPrefix(line, "#") {
+				inputs = append(inputs, line)
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+	}
+
+	// Добавляем из CLI аргумента если указан флаг -i
+	if inputArg != "" {
+		inputs = append(inputs, inputArg)
+	}
+
+	// Если нет других источников, читаем из stdin
+	if len(inputs) == 0 {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
+			if line != "" {
+				inputs = append(inputs, line)
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
+	}
+
+	return inputs, nil
+}
+
+// extractDomain извлекает доменное имя из результата
+func extractDomain(result string) string {
+	parts := strings.Fields(result)
+	if len(parts) >= 3 {
+		return parts[2] // [TYPE] IP DOMAIN
+	}
+	return ""
+}
+
+// writeOutput записывает результат в консоль и файл (если указан)
+func writeOutput(output string, outputFile *os.File) {
+	fmt.Println(output)
+	if outputFile != nil {
+		outputFile.WriteString(output + "\n")
+	}
+}
+
 // This function grabs the SSL certificate, then dumps the SAN and CommonName
 func sslChecks(ip string, resChan chan<- string, client *http.Client) {
 
@@ -29,6 +171,7 @@ func sslChecks(ip string, resChan chan<- string, client *http.Client) {
 
 	req, reqErr := http.NewRequest("HEAD", url, nil)
 	if reqErr != nil {
+		log.Printf("SSL check failed for %s: %v", ip, reqErr)
 		return
 	}
 
@@ -36,11 +179,12 @@ func sslChecks(ip string, resChan chan<- string, client *http.Client) {
 	if clientErr != nil {
 		return
 	}
+	defer resp.Body.Close() // Добавить закрытие
 
 	if resp.TLS != nil && len(resp.TLS.PeerCertificates) > 0 {
 		dnsNames := resp.TLS.PeerCertificates[0].DNSNames
 		for _, name := range dnsNames {
-			resChan <- "[SSL-SAN] " + ip + " " + string(name)
+			resChan <- "[SSL-SAN] " + ip + " " + name
 		}
 		resChan <- "[SSL-CN] " + ip + " " + resp.TLS.PeerCertificates[0].Subject.CommonName
 	}
@@ -48,6 +192,9 @@ func sslChecks(ip string, resChan chan<- string, client *http.Client) {
 
 // Do a DNS PTR lookup on the IP
 func dnsChecks(ip string, resChan chan<- string, resolver *net.Resolver) {
+	if resolver == nil {
+		resolver = net.DefaultResolver
+	}
 
 	addr, err := resolver.LookupAddr(context.Background(), ip)
 	if err != nil {
@@ -59,7 +206,7 @@ func dnsChecks(ip string, resChan chan<- string, resolver *net.Resolver) {
 	}
 }
 
-func worker(jobChan <-chan string, resChan chan<- string, wg *sync.WaitGroup, transport *http.Transport, client *http.Client, resolver *net.Resolver) {
+func worker(jobChan <-chan string, resChan chan<- string, wg *sync.WaitGroup, client *http.Client, resolver *net.Resolver) {
 	defer wg.Done()
 
 	for job := range jobChan {
@@ -68,16 +215,69 @@ func worker(jobChan <-chan string, resChan chan<- string, wg *sync.WaitGroup, tr
 	}
 
 }
+
 func main() {
-	workers := flag.Int("t", 32, "numbers of threads")
-	resolverIP := flag.String("r", "", "IP of DNS resolver for lookups")
-	dnsProtocol := flag.String("protocol", "udp", "Protocol for DNS lookups (tcp or udp)")
-	resolverPort := flag.Int("p", 53, "Port to bother the specified DNS resolver on")
+	listFile := flag.String("l", "", "File containing list of IP addresses/CIDR blocks")
+	inputArg := flag.String("i", "", "Single IP address or CIDR block")
+	outputFile := flag.String("o", "", "Save stdout to file")
+	helpFlag := flag.Bool("h", false, "Show help message")
+	silentFlag := flag.Bool("s", false, "Output only domain names")
+
+	// Добавляем длинные флаги
+	flag.StringVar(listFile, "list", "", "File containing list of IP addresses/CIDR blocks")
+	flag.StringVar(inputArg, "input", "", "Single IP address or CIDR block")
+	flag.StringVar(outputFile, "output", "", "Save stdout to file")
+	flag.BoolVar(helpFlag, "help", false, "Show help message")
+	flag.BoolVar(silentFlag, "silent", false, "Output only domain names")
+
 	flag.Parse()
 
-	scanner := bufio.NewScanner(os.Stdin)
-	jobChan := make(chan string)
-	resChan := make(chan string)
+	// Обработка справки
+	if *helpFlag {
+		printUsage()
+		os.Exit(0)
+	}
+
+	// Читаем входные данные
+	inputs, err := readInputs(*listFile, *inputArg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Если нет входных данных, показываем справку
+	if len(inputs) == 0 {
+		printUsage()
+		os.Exit(0)
+	}
+
+	// Расширяем CIDR блоки в отдельные IP адреса
+	var allIPs []string
+	for _, input := range inputs {
+		ips, err := expandCIDR(input)
+		if err != nil {
+			log.Printf("Error processing %s: %v", input, err)
+			continue
+		}
+		allIPs = append(allIPs, ips...)
+	}
+
+	if len(allIPs) == 0 {
+		log.Fatal("No valid IP addresses found")
+	}
+
+	// Открываем файл для записи если указан
+	var outFile *os.File
+	if *outputFile != "" {
+		var err error
+		outFile, err = os.Create(*outputFile)
+		if err != nil {
+			log.Fatalf("Cannot create output file %s: %v", *outputFile, err)
+		}
+		defer outFile.Close()
+	}
+
+	jobChan := make(chan string, 100)
+	resChan := make(chan string, 100)
 	done := make(chan struct{})
 
 	// Set up TLS transport
@@ -90,43 +290,30 @@ func main() {
 	}
 
 	// Set up HTTP client
-	var client = &http.Client{
+	client := &http.Client{
 		Timeout:   time.Second * 10,
 		Transport: transport,
 	}
 
 	// Set up DNS resolver
-	var resolver *net.Resolver
-
-	if *resolverIP != "" {
-		resolver = &net.Resolver{
-			PreferGo: true,
-			Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
-				d := net.Dialer{}
-				return d.DialContext(ctx, *dnsProtocol, fmt.Sprintf("%s:%d", *resolverIP, *resolverPort))
-			},
-		}
-	}
+	resolver := net.DefaultResolver
 
 	var wg sync.WaitGroup
-	wg.Add(*workers)
+	wg.Add(defaultWorkers)
 
 	go func() {
 		wg.Wait()
 		close(done)
 	}()
 
-	for i := 0; i < *workers; i++ {
-
-		go worker(jobChan, resChan, &wg, transport, client, resolver)
+	for i := 0; i < defaultWorkers; i++ {
+		go worker(jobChan, resChan, &wg, client, resolver)
 	}
 
+	// Отправляем IP адреса в канал
 	go func() {
-		for scanner.Scan() {
-			jobChan <- scanner.Text()
-		}
-		if err := scanner.Err(); err != nil {
-			log.Println(err)
+		for _, ip := range allIPs {
+			jobChan <- ip
 		}
 		close(jobChan)
 	}()
@@ -136,10 +323,15 @@ func main() {
 		case <-done:
 			return
 		case res := <-resChan:
-			if strings.HasSuffix(res, ".") {
-				res = res[:len(res)-1]
+			res = strings.TrimSuffix(res, ".")
+			if *silentFlag {
+				domain := extractDomain(res)
+				if domain != "" {
+					writeOutput(domain, outFile)
+				}
+			} else {
+				writeOutput(res, outFile)
 			}
-			fmt.Println(res)
 		}
 	}
 }
